7ddb026060f44fff558aca2ea1a3c58a756227c1
seolhyelin <tosiltosilkk@gmail.com>
2022-10-10 22:29:08 +0900

    Feat: 기초..재밌당..

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
+}
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
+// loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
