c0894ac8966f7a06f2bb1bd12582f3f8cac8202c
seolhyelin <tosiltosilkk@gmail.com>
2022-10-11 22:51:53 +0900

    Feat: localstorage ..기쵸

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  const username = loginInput.value;
+
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 }
 
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
+function paintGreetings(username) {
+  greeting.innerText = `hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-// loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  //그리팅 숨기기
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  //그리팅 보여주기
+  paintGreetings(savedUsername);
+}
