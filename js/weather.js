56f5a1afc765d05d7ed9b53e842968f3ccbd56cb
seolhyelin <hyelin@bfactory.inc>
2022-10-18 10:49:02 +0900

    Feat: 모멘텀 기능 구현

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const APIKey = "1430a4b039126806b68f52c021331326";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.latitude;
-  console.log("i live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError() {
   alert("can't find you");
